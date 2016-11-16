2287129f538322da97459e9dd483451e0bc72a4a
SjoerdJakobs <18284@ma-web.nl>
2016-11-16 16:33:40 +0100

    island paradise!!
    island paradise!!

---
+++ Assets/Scripts/GenerateSmoothTerrainEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(CreateSmoothTerrain))]
+public class GenerateSmoothTerrainEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        CreateSmoothTerrain terrainGen = (CreateSmoothTerrain)target;
+        if (DrawDefaultInspector())
+        {
+
+        }
+        if (GUILayout.Button("generate"))
+        {
+            terrainGen.Generate();
+        }
+    }
+}
